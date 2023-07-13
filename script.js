0371ee482da7a62175ab5a730fb98e3529bb2ed5
Omar Cruz Rmz <92276635+Damanger@users.noreply.github.com>
2023-07-13 04:17:10 -0600

    Update script.js
    updating

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ toggle.addEventListener('change',(event)=>{
     let checked = event.target.checked;
     document.body.classList.toggle('dark');
     if(checked){
-        label_toggle.innerHTML = '<img src="./sun.png" alt="Imagen del modo oscuro">';
+        label_toggle.innerHTML = '<img src="./sun.png" alt="Dark mode image">';
         label_toggle.style.width = '4rem';
         label_toggle.style.height = '4rem';
     } else{
-        label_toggle.innerHTML = '<img src="./moon.png" alt="Imagen del modo oscuro">';
+        label_toggle.innerHTML = '<img src="./moon.png" alt="Dark mode image">';
         label_toggle.style.width = '4rem';
         label_toggle.style.height = '4rem';
     }
@@ -94,3 +94,69 @@ form.addEventListener('submit', function(event) {
         });
     });
 });
+
+//Hace un efecto de srcamble
+window.onload = function () {
+    const links = document.querySelectorAll("a.cipher");
+    const solveMilliseconds = 500;
+    const characterSelectionMilliseconds = 40;
+    const delayMilliseconds = 200;
+    const characters = [..."ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890+#@/*%&<>^"];
+  
+    const randomArrayElement = (arr) => {
+      return arr[(arr.length * Math.random()) | 0];
+    };
+  
+    links.forEach((element) => {
+      element.addEventListener("mouseenter", (e) => {
+        const element = e.target;
+        scrambleText(element);
+        e.preventDefault();
+      });
+    });
+  
+    function scrambleText(element) {
+      if (element.classList.contains("active") == false) {
+        let delay = 0;
+        const elementText = element.innerText;
+        const elementCharacters = [...elementText];
+        const lockMilliseconds =
+          delayMilliseconds * elementCharacters.length + solveMilliseconds;
+  
+        element.classList.add("active");
+  
+        setTimeout(() => {
+          element.classList.remove("active");
+        }, lockMilliseconds);
+  
+        elementCharacters.forEach((character, index) => {
+          setTimeout(
+            () => {
+              let intervalId = setInterval(() => {
+                const randomCharacter = randomArrayElement(characters);
+                element.innerText = replaceCharacter(
+                  element.innerText,
+                  index,
+                  randomCharacter
+                );
+  
+                setTimeout(() => {
+                  clearInterval(intervalId);
+                  element.innerText = replaceCharacter(
+                    element.innerText,
+                    index,
+                    elementCharacters[index]
+                  );
+                }, solveMilliseconds);
+              }, characterSelectionMilliseconds);
+            },
+            delay === 0 ? (delay += 1) : (delay += delayMilliseconds)
+          );
+        });
+      }
+    }
+  
+    function replaceCharacter(str, index, chr) {
+      return `${str.substring(0, index)}${chr}${str.substring(index + 1)}`;
+    }
+};
